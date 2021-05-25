b01e8e26ddcbe911a2cc580befc9380703fe2f3f
marcoBrighterAI <marco@brighter.ai>
2021-05-25 20:59:07 +0200

    feat: provide the location as an argument when deploying the webapp

--- commands.sh
+++ commands.sh
@@ -4,4 +4,4 @@
 az group create --name flask-ml-azure-serverless-rg --location westeurope --tags udacity=udacity-project2
 
 # Create and deploy the App Service app
-az webapp up --sku F1 --name flask-ml-service-marco -g udacity-project-2-rg
+az webapp up --sku F1 --name flask-ml-service-marco -g udacity-project-2-rg -l westeurope
