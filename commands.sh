37040a9953b45fc68894383842556cf1c2c0f89e
marcoBrighterAI <marco@brighter.ai>
2021-05-25 20:46:04 +0200

    fix: change service connection name to match the correct resource group

--- commands.sh
+++ commands.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Create a Resource Group
-az group create --name udacity-project-2-rg --location westeurope --tags udacity=udacity-project2
+az group create --name flask-ml-azure-serverless-rg --location westeurope --tags udacity=udacity-project2
 
 # Create and deploy the App Service app
 az webapp up --sku F1 --name flask-ml-service-marco -g udacity-project-2-rg
